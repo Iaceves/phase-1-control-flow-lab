d6ef379eb682e751d6a7a5f52a589dbe821a5d72
iaceves <acevesaceves97@gmail.com>
2022-08-28 21:33:41 -0400

    commit tree 37c5a689214abe4d4947d9c6aaf2c626838003f9

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
-function scuberGreetingForFeet(){
+  
+
+function scuberGreetingForFeet(feet) {
   // Write your code here!
+  let result;
+
+  if (feet > 2500 ){
+    result = 'No can do.'
+  }
+  else if(feet >= 2001){
+    result = 'I will gladly take your thirty bucks.'
+  }
+  else if (feet >= 401) {
+    result = 'That will be twenty bucks.'
+  }
+  else if (feet <= 400){
+    result = 'This one is on me!'
+  }
+  return result;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return (city === "NYC"  ? "Ok, sounds good." : "No go.");
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+let generous = 'generous';
+let notGenerous = 'not as generous';
+let anyThing = 'thanks for everything';
+let result;
+
+  switch (tip) {
+    case generous: 
+     result = "Thank you so much.";
+      break;
+    case notGenerous:
+      result = "Thank you."
+      break;    
+    case anyThing:
+      result = "Bye."
+      break;
+    
+  }
+  return result;
 }
